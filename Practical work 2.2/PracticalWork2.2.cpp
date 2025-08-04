e225f80050d1338e5e208fa8a664ffb0ef3f5317
IlshatYusupov <yusupovilshatilus@gmail.com>
2025-08-04 06:08:02 +0300

    Create PracticalWork2.2.cpp
    Первая редакция PracticalWork2.2

---
+++ Practical work 2.2/PracticalWork2.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int price = 100;
+    int delivery = 20;
+    int discount = 10;
+    int fullPrice = price + delivery - discount;
+
+    std::cout << "===================\n";
+    std::cout << "Кальклятор полной истоимости товара\n";
+    std::cout << "===================\n";
+    std::cout << "Стоимость без доставки и скидки: +" << price << "\n";
+    std::cout << "Доставка: +" << delivery << "\n";
+    std::cout << "Скидка: -" << discount << "\n";
+    std::cout << "-------------------\n";
+    std::cout << "Полная стоимость товара: " << fullPrice << "\n";
+}
